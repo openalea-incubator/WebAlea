3fd6ed21067e648df8baf10ab9763f1c3d8dd10c
RAAPOTO Enzo <enzo.raapoto@etu.univ-orleans.fr>
2025-10-01 16:22:32 +0200

    edited starting script

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Launch the backend and frontend using Docker Compose
-
-docker-compose up --build -d
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Stop and clean up Docker Compose services
-
-docker-compose down --volumes --remove-orphans
