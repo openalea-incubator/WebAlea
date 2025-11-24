c854989b66e05fea0ad9c671dd927710340dcdda
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-11-24 21:43:38 +0100

    Fix CustomNode status

--- webAleaFront/src/components/workspace/CustomNode.jsx
+++ webAleaFront/src/components/workspace/CustomNode.jsx
@@ -79,7 +79,7 @@ export default function CustomNode(nodeProps) {
                         background: borderColor,
                         cursor: "pointer",
                     }}
-                    title={`status: ${label}`}
+                    title={`status: ${status}`}
                     onClick={handleStatusClick}
                 />
                 <strong style={{ fontSize: 13 }}>{label}</strong>
