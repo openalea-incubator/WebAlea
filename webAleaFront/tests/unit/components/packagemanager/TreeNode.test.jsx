2e10f60a1c73ab31218c326a1fe78e801b162cec
KillianCARRAT <killian.carrat@etu.univ-orleans.fr>
2026-01-06 14:32:29 +0100

    Test update

---
+++ webAleaFront/tests/unit/components/packagemanager/TreeNode.test.jsx
@@ -0,0 +1,28 @@
+import TreeNode from '../../../../src/components/packagemanager/TreeNode.jsx';
+import { expect, test, describe } from '@jest/globals';
+
+
+describe('TreeNode', () => {
+    const node = { id: 1, data: { label: "Test node" } };
+    const treeNode = new TreeNode(node);
+
+    test("Initialize Node", () => {
+        expect(treeNode.node).toBe(node);
+    });
+
+    test("Serialize Node", () => {
+        expect(treeNode.serialize()).toEqual({id: 1, label: "Test node", node: node });
+    });
+
+    test("Serialize extra value", () => {
+       let new_node = {id: 42, data: {label : "Test extra value"}, extra: "ingored test"};
+       let new_treeNode = new TreeNode(new_node);
+
+       let serialized = new_treeNode.serialize();
+
+       expect(new_treeNode.serialize()).toEqual(serialized);
+       expect(serialized.id).toBe(42);
+       expect(serialized.label).toBe("Test extra value");
+       expect(serialized.node).toBe(new_node);
+    });
+})
